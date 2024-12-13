10b071e46ae421b69f61f4c36de348b0c9a34262
Gabriel Ibenye <gabrielibenye@gmail.com>
2024-12-13 13:57:15 -0500

    removed unneccessary tests

--- tests/ErcaspayTest.php
+++ tests/ErcaspayTest.php
@@ -8,11 +8,6 @@ beforeEach(function () {
     $this->callPrivateMethod($this->ercaspay, 'initializeClient', false);
 });
 
-it('throws exception when required fields are not provided', function () {
-    expect(fn () => $this->ercaspay->initiateTransaction([]))
-        ->toThrow(InvalidArgumentException::class);
-});
-
 it('generates a payment reference in UUID format', function () {
     $paymentRef = $this->ercaspay->generatePaymentReferenceUuid();
     expect($paymentRef)
@@ -20,14 +15,3 @@ it('generates a payment reference in UUID format', function () {
         ->toMatch('/[0-9a-fA-F\-]{36}/'); // Matches UUID format
 });
 
-it('throws an exception if a required paraeeter are missing', function () {
-    // Act & Assert: Check each required field validation
-    expect(fn () => $this->ercaspay->fetchTransactionStatus('       ', 'paymentReference', 'paymentMethod'))
-        ->toThrow(IsNullException::class);
-
-    expect(fn () => $this->ercaspay->verifyTransaction(''))
-        ->toThrow(IsNullException::class);
-
-    expect(fn () => $this->ercaspay->initiateUssdTransaction('transactionRef', ''))
-        ->toThrow(IsNullException::class);
-});
